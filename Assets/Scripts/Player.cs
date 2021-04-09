fc394c1952af65c8a7d4184ae20280641ea67ca3
kpblcke <kpblcke@gmail.com>
2021-04-09 15:39:26 +0400

    -add movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,20 +5,28 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
-
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+    
     Rigidbody2D _rigidBody;
-
     Animator _animator;
+    Collider2D _collider2D;
+    
+    float gravityScaleAtStart;
 
 	// Use this for initialization
 	void Start () {
         _rigidBody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        _collider2D = GetComponent<Collider2D>();
+        gravityScaleAtStart = _rigidBody.gravityScale;
 	}
 
 	// Update is called once per frame
 	void Update () {
         Run();
+        ClimbLadder();
+        Jump();
         FlipSprite();
 	}
 
@@ -29,6 +37,38 @@ public class Player : MonoBehaviour
         _rigidBody.velocity = playerVelocity;
         bool playerHasHorizontalSpeed = Mathf.Abs(_rigidBody.velocity.x) > Mathf.Epsilon;
         _animator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+    
+    private void ClimbLadder()
+    {
+	    if (!_collider2D.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+	    {
+		    _animator.SetBool("Climbing", false);
+		    _rigidBody.gravityScale = gravityScaleAtStart;
+		    return;
+	    }
+
+	    float controlThrow = Input.GetAxis("Vertical");
+	    Vector2 climbVelocity = new Vector2(_rigidBody.velocity.x, controlThrow * climbSpeed);
+	    _rigidBody.gravityScale = 0;
+	    _rigidBody.velocity = climbVelocity;
+
+	    bool playerHasVerticalSpeed = Mathf.Abs(_rigidBody.velocity.y) > Mathf.Epsilon;
+	    _animator.SetBool("Climbing", playerHasVerticalSpeed);
+
+    }
+    
+    private void Jump()
+    {
+	    if (!_collider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+	    {
+		    return;
+	    } 
+	    if (Input.GetButtonDown("Jump"))
+	    {
+		    Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+		    _rigidBody.velocity += jumpVelocityToAdd;
+	    }
     }
 
     private void FlipSprite()
